2f379455713e39928f9588a1a627bc8477ef862f
bbmkastan <60830590+bbmkastan@users.noreply.github.com>
2021-06-06 15:11:18 -0400

    Update App.java

--- src/main/java/base/App.java
+++ src/main/java/base/App.java
@@ -3,7 +3,7 @@ package base;
 import java.util.Scanner;
 
 /*
- *  UCF COP3330 Summer 2021 Assignment 1 Solution
+ *  UCF COP3330 Summer 2021 Assignment 2 Solution
  *  Copyright 2021 Bao Kastan
  */
 
